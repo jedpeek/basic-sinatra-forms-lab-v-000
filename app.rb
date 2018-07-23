3aaf079caad0ba9c208a1d0ae014ed171963f9a0
Jed Peek <jedpeek@yahoo.com>
2018-07-23 23:11:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,15 +4,17 @@ class App < Sinatra::Base
 
   post '/team' do
     params.inspect
+    erb :team
   end
+  
+    get '/team' do
+      erb :team
+    end
 
   get '/newteam' do
     erb :newteam
   end
 
-  get '/team' do
-    erb :team
-  end
 
 
 end
